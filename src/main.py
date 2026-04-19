e16351f485a03d2195cdde1e6c81d660228d43ba
Platonn3 <platon.kurbatov87@gmail.com>
2026-04-19 22:35:07 +0500

    add cors

--- src/main.py
+++ src/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 
+from fastapi.middleware.cors import CORSMiddleware
+
 from src.auth.router import router as auth_router
 from src.events.router import router as events_router
 from src.photos.router import router as photos_router
 from src.users.router import router as users_router
 
 app = FastAPI(title="EventSnap", version="0.1.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(auth_router)
 app.include_router(users_router)
